153271448d2a1eb11d40c231abbd3d5992062702
Jacob Prudent <jake.prudent@gmail.com>
2020-06-04 23:01:09 -0700

    MVP

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,12 +1,25 @@
 # Your code here
 
 
-
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
     # Your code here
+    filenames_dict = {}
+    result = []
+    for filename in files:
+        words = filename.split("/")
+        # the last word is a file name
+        if words[-1] not in filenames_dict:
+            filenames_dict[words[-1]] = [filename]
+        else:
+            filenames_dict[words[-1]].append(filename)
+
+    for query in queries:
+        if query in filenames_dict:
+            for f in filenames_dict[query]:
+                result.append(f)
 
     return result
 
